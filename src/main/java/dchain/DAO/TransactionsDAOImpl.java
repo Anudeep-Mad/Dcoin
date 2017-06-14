5681eb80380e1de3b4234c11c9d7ea217b4732e1
Anudeep Medishetti <medishettianudeep@gmail.com>
2017-06-14 10:44:31 +0530

    Revert "Transactions Complete"
    This reverts commit 018d7eed5e3413343d7851245aae8e6c10658638.

--- src/main/java/dchain/DAO/TransactionsDAOImpl.java
+++ src/main/java/dchain/DAO/TransactionsDAOImpl.java
@@ -4,13 +4,12 @@ import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Timestamp;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-import com.mongodb.Mongo;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 
 import dchain.Transactions;
 
@@ -33,21 +32,31 @@ public class TransactionsDAOImpl implements TransactionsDAO{
 
 	@Override
 	public void newTransactions(Transactions transactions) throws NoSuchAlgorithmException, UnknownHostException {
-		@SuppressWarnings("deprecation")
-		MongoTemplate mongoTemplate = new MongoTemplate(new Mongo(), "test");
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		String blockid = generateHash(transactions.getFrom_name(),transactions.getTo_name(),transactions.getAmount(),timestamp.toString());
-		transactions.setBlockId(blockid);
-		if (!mongoTemplate.collectionExists("Transactions")) {
-			mongoTemplate.createCollection("Transactions");
+		MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+		
+        // Now connect to your databases
+        @SuppressWarnings("deprecation")
+		DB db = mongoClient.getDB( "test" );
+        System.out.println("Connect to database successfully");
+			
+        DBCollection coll = db.getCollection("trans");
+        System.out.println("Collection trans selected successfully");
+			
+        BasicDBObject doc = new BasicDBObject("title", "Dcoin").
+                append("From", transactions.getFrom_name()).
+                append("To", transactions.getTo_name()).
+                append("Amount", transactions.getAmount()).
+                append("Hash", generateHash(transactions.getFrom_name(),transactions.getTo_name(),transactions.getAmount()));
+				
+        coll.insert(doc);
+        System.out.println("Document inserted successfully");
+        mongoClient.close();
 		}
-		mongoTemplate.insert(transactions, "Transactions");
 	
-	}	
 
-	private String generateHash(String from_name, String to_name, String amount,String timestamp) throws NoSuchAlgorithmException {
+	private String generateHash(String from_name, String to_name, String amount) throws NoSuchAlgorithmException {
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		String concat_trans = from_name+to_name+amount+timestamp;
+		String concat_trans = from_name+to_name+amount;
 		byte[] hash = digest.digest(concat_trans.getBytes(StandardCharsets.UTF_8));
 		return hash.toString();
 	}
